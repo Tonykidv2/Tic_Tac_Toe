61a2ebda2f555010b967f44ae794c80f1cb1c462
Anthony Ramoslebron <ajramoslebron@gmail.com>
2017-08-27 16:51:15 -0400

    Cleaning Code

--- app/src/main/java/edu/fullsail/mgems/cse/tic_tac_toe/MainActivity.java
+++ app/src/main/java/edu/fullsail/mgems/cse/tic_tac_toe/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     int SelectDp;
     int TitleDp;
     int SubDp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
